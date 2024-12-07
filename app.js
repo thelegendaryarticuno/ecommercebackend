af44c86284b174d7321096b4d3b64bbb67b04e90
Naman Mani <142525587+thelegendaryarticuno@users.noreply.github.com>
2024-12-07 13:50:31 +0530

    updated

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'https://ecommercefrontend-mocha.vercel.app', // Frontend URL
+  origin: ' http://localhost:5173', // Frontend URL
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
