34d6a412cd7162a67dbfe7083caf6707cb4b3d64
Naman Mani <142525587+thelegendaryarticuno@users.noreply.github.com>
2025-01-07 01:26:42 +0530

    push

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:3000','https://merabestie.com','https://merabestie-five.vercel.app'], 
+  origin: ['http://localhost:3000','https://www.merabestie.com','https://merabestie-five.vercel.app'], 
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
